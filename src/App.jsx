46ae97a8f73c2c2ae76a8296a551c38f2883f423
Misael caro <mishaelcaro@gmail.com>
2022-11-27 22:37:32 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import './App.css'
+import WeatherCard from './components/WeatherCard'
+
+
+
+
+function App() {
+  const [coords, setCoords] = useState()
+  const [weather, setWeather] = useState()
+  const [temperature, setTemperature] = useState()
+  const [isCelsius, setIsCelsius] = useState(true)
+
+
+  const success = (pos) => {
+    const newCoords = {
+      lat: pos.coords.latitude,
+      lon: pos.coords.longitude
+    }
+    setCoords(newCoords)
+  }
+
+  const changeUnitTemperature = () => {
+    setIsCelsius(!isCelsius)
+
+  }
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(success)
+  }, [])
+
+  useEffect(() => {
+    if (coords) {
+      const API_KEY = "f4242e9427fbc4148e47904dfd242342"
+      const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${API_KEY}`
+      axios.get(URL)
+        .then(res => {
+          const tempKelvin = res.data.main.temp
+          const tempCelsius = (tempKelvin - 273.15).toFixed(1)
+          const tempFahrenheit = ((tempCelsius * 9 / 5) + 32).toFixed(1)
+          const newTemperature = {
+            celsius: tempCelsius,
+            fahrenheit: tempFahrenheit
+          }
+          setTemperature(newTemperature)
+          setWeather(res.data)
+        })
+
+        .catch(err => console.log(err))
+
+    }
+  }, [coords])
+
+
+
+  return (
+
+
+
+    <div className="app" >
+      
+      {
+
+        weather ? (
+          <WeatherCard
+            weather={weather}
+            temperature={temperature}
+            changeUnitTemperature={changeUnitTemperature}
+            isCelsius={isCelsius}
+          />
+        ) : <span className="spiner"></span>
+
+      }
+
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/WeatherCard.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+
+
+
+const WeatherCard = ({ weather, temperature, isCelsius, changeUnitTemperature,}) => {
+    let url = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`
+    
+    if (url == `http://openweathermap.org/img/wn/01d@4x.png`)
+        url = `https://i.ibb.co/wgJLRt6/Dise-o-sin-t-tulo-3.png`;
+
+    else if (url == `http://openweathermap.org/img/wn/01n@4x.png`)
+        url = `https://i.ibb.co/jkYsjD4/Dise-o-sin-t-tulo-4.png`
+
+    else if (url == `http://openweathermap.org/img/wn/02d@4x.png`)
+        url = `https://i.ibb.co/c2ZLKM2/Dise-o-sin-t-tulo-5.png`
+
+    else if (url == `http://openweathermap.org/img/wn/02n@4x.png`)
+        url = `https://i.ibb.co/6Wq939T/Dise-o-sin-t-tulo-6.png`
+
+    else if (url == `http://openweathermap.org/img/wn/03d@4x.png`)
+        url = `https://i.ibb.co/qRKXHpH/Dise-o-sin-t-tulo-7.png`
+
+    else if (url == `http://openweathermap.org/img/wn/03n@4x.png`)
+        url = `https://i.ibb.co/qRKXHpH/Dise-o-sin-t-tulo-7.png`
+
+    else if (url == `http://openweathermap.org/img/wn/04d@4x.png`)
+        url = `https://i.ibb.co/qRKXHpH/Dise-o-sin-t-tulo-7.png`
+
+    else if (url == `http://openweathermap.org/img/wn/04n@4x.png`)
+        url = `https://i.ibb.co/qRKXHpH/Dise-o-sin-t-tulo-7.png`
+
+    else if (url == `http://openweathermap.org/img/wn/09d@4x.png`)
+        url = `"https://i.ibb.co/RSVQRnq/Dise-o-sin-t-tulo-9.png`
+
+    else if (url == `http://openweathermap.org/img/wn/09n@4x.png`)
+        url = `https://i.ibb.co/PZ8GWtJ/Dise-o-sin-t-tulo-10.png`
+
+    else if (url == `http://openweathermap.org/img/wn/10d@4x.png`)
+        url = `https://i.ibb.co/d0BgDXQ/Dise-o-sin-t-tulo-8.png`
+
+    else if (url == `http://openweathermap.org/img/wn/10n@4x.png`)
+        url = `https://i.ibb.co/zVpm9KJ/Dise-o-sin-t-tulo-11.png`
+
+    else if (url == `http://openweathermap.org/img/wn/11d@4x.png`)
+        url = `https://i.ibb.co/4TRnm5x/Dise-o-sin-t-tulo-12.png`
+
+    else if (url == `http://openweathermap.org/img/wn/11n@4x.png`)
+        url = `https://i.ibb.co/7tnTFH0/Dise-o-sin-t-tulo-13.png`
+
+    else if (url == `http://openweathermap.org/img/wn/13d@4x.png`)
+        url = `https://i.ibb.co/6gNZ0dw/Dise-o-sin-t-tulo-16.png`
+
+    else if (url == `http://openweathermap.org/img/wn/13n@4x.png`)
+        url = `https://i.ibb.co/TkwRDy6/Dise-o-sin-t-tulo-15.png`
+
+    else if (url == `http://openweathermap.org/img/wn/50d@4x.png`)
+        url = `https://i.ibb.co/XC1gC6y/Dise-o-sin-t-tulo-17.png`
+
+    else if (url == `http://openweathermap.org/img/wn/50n@4x.png`)
+        url = `https://i.ibb.co/XC1gC6y/Dise-o-sin-t-tulo-17.png`
+
+
+    return (
+        <article className="weathercard">
+           
+
+            <h1 className="title">Weather App</h1>
+            <h3 className="subTitle">{`${weather.name}, ${weather.sys.country}`}</h3>
+
+            <p className="temp">{isCelsius ? `${temperature.celsius} °C` : `${temperature.fahrenheit} °F`} </p>
+            <section className="weatherCard-body">
+                <div>
+
+                    <img className="imagen" src={url} alt="" />
+                    <ul  >
+                    <li className="description" >{weather.weather[0].description}</li>
+                    </ul>
+                </div>
+                <hr className="divider"/>
+                <ul>
+                    
+                    <li>Wind speed: {weather.wind.speed} m/s</li>
+                    <br />
+                    <li>Clouds: {weather.clouds.all}%</li>
+                    <br />
+                    <li>pressure: {weather.main.pressure} hpa</li>
+                </ul>
+            </section>
+            <br />
+            <br />
+            <button className="weatherCard-button" onClick={changeUnitTemperature}>Degrees °F/°C</button>
+           
+        </article>
+            
+    )
+}
+
+export default WeatherCard
